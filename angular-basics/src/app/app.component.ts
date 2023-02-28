c2470a4e12073c40c31ca5f48ff3e9604e8d499a
un-know-n <fa1thlezss@gmail.com>
2023-02-28 13:58:06 +0200

    Cleaned

--- angular-basics/src/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-//import productsJson from './data/products.json';
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-})
-export class AppComponent {}
